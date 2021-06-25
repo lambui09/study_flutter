cc8dbba98d834aa760fc0f1870061a2eb925010f
Bui Duc Lam <buiduclam09@gmail.com>
2021-06-25 19:58:09 +0700

    view in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,21 @@ class Application extends StatefulWidget {
 
 class _ApplicationState extends State<Application> {
   String text = "lam bui";
+  bool showValue = false;
+  int groupView = 0;
+  double seekbar = 0.0;
+  void medthoD(value) {
+    setState(() {
+      groupView = value;
+      print(value);
+    });
+  }
+
+  void setDataSeekbar(value) {
+    setState(() {
+      seekbar = value;
+    });
+  }
 
   List<int> _items = new List();
   @override
@@ -19,32 +34,71 @@ class _ApplicationState extends State<Application> {
   }
 
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        backgroundColor: Colors.green,
-        title: new Text("Bui Duc Lam"),
-        leading: new Icon(Icons.menu),
-        centerTitle: true,
-        actions: <Widget>[
-          new IconButton(
-              icon: new Icon(Icons.arrow_forward),
-              onPressed: () {
-                setState(() {
-                  text = "text test";
-                });
-              }),
-          new IconButton(
-              icon: new Icon(Icons.close),
-              onPressed: () {
-                setState(() {
-                  text = "close buttom";
-                });
-              })
-        ],
-      ),
-      body: new Center(
-        child: new Text(text),
-      ),
-    );
+    return new MaterialApp(
+        title: 'Dummy Application',
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            title: Text('Dumy Application'),
+          ),
+          body: Column(
+            children: <Widget>[
+              TextField(
+                onSubmitted: (String tx) {
+                  setState(() {
+                    text = tx;
+                  });
+                },
+                decoration: InputDecoration(
+                    hintText: 'Type something', labelText: 'Fullname'),
+              ),
+              Text(text),
+              RaisedButton(
+                onPressed: null,
+                child: Text('Raised Button'),
+              ),
+              FlatButton(onPressed: null, child: Text('lamsfasdfds')),
+              Checkbox(
+                  value: this.showValue,
+                  onChanged: (bool value) {
+                    setState(() {
+                      this.showValue = value;
+                      print(value);
+                    });
+                  }),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Radio(
+                      value: 1,
+                      groupValue: groupView,
+                      onChanged: (int value) {
+                        medthoD(value);
+                      }),
+                  Radio(
+                      value: 2,
+                      groupValue: groupView,
+                      onChanged: (int value) {
+                        medthoD(value);
+                      }),
+                  Radio(
+                      value: 3,
+                      groupValue: groupView,
+                      onChanged: (int value) {
+                        medthoD(value);
+                      })
+                ],
+              ),
+              Slider(
+                  value: 5.0,
+                  min: 1.0,
+                  max: 10.0,
+                  onChanged: (double value) {
+                    setDataSeekbar(value);
+                  }),
+              Text("This is value : $seekbar")
+            ],
+          ),
+        ));
   }
 }
